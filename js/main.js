8e487eaec435eca873f39508110cfa953675781f
Anthony Oliveira <anthony.silvaoliveira@outlook.com>
2025-09-16 22:11:40 -0300

    refactor: path service worker

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceWorker.js')
+    navigator.serviceWorker.register('./serviceWorker.js')
       .then(reg => console.log('SW registrado:', reg))
       .catch(err => console.log('Falha ao registrar SW:', err));
   });
